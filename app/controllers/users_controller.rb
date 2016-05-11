abd2dfb58e9cc807408612e25d6ff8646ef57b70
koujitanaka <tanaka@energyfactory-japan.com>
2016-05-11 02:48:54 +0000

    kadai4-1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,40 @@
 class UsersController < ApplicationController
-  
-  def show # 追加
-   @user = User.find(params[:id])
+
+  def show
+    @user = User.find(params[:id])
   end
-  
+
   def new
     @user = User.new
   end
-  
+
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to the Sample App!" 
-      redirect_to @user 
+      log_in @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
       render 'new'
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # 更新に成功したときの処理
+    else
+      render 'edit'
+    end
+  end
+
   private
 
-  def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
-
