6ea5df8da07ff7ac7cb299545cf0c8b925284bac
koujitanaka <tanaka@energyfactory-japan.com>
2016-05-12 10:49:54 +0000

    kadai4-6

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:create, :edit, :update]
+  before_action :set_user, only: [:edit, :update]
   before_action :collect_user, only: [:edit, :update]
 
   def show
